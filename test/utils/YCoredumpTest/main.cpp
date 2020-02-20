e43bf2f3e2a740ef0a905c5ac26997e0b81b5e48
leaf_yyl <leaf_yyl@163.com>
2020-02-21 00:34:03 +0800

    add notes

--- test/utils/YCoredumpTest/main.cpp
+++ test/utils/YCoredumpTest/main.cpp
@@ -12,6 +12,25 @@
 
 #include <QCoreApplication>
 
+/* Coredump
+ * 1: use 'ulimit -c' to check if coredump file is auto saved,
+ *    if zero is returned, use 'ulimit -c size' to
+ *    enable coredump file auto saving with specified size;
+ * 2: use 'echo "/opt/usr/core/core-%e-%t" > /proc/sys/kernel/core_pattern'
+ *    to assign coredump file location and file name. Detailed description
+ *    for coredump file format is specified in output of 'man core';
+ * 3: use 'gdb -c coredump_file ./program' to restore crash stack.
+ *
+ * signal + Objdump + c++filt   // need more study
+ * 1: register user function to catch SIGSEGV signal and print call stack by using
+ *    backtrace and backtrace_symbols functions;
+ * 2: use 'objdump -d program > output_file' to disassemble program into output file;
+ * 3: search locations printed in call stack when program crashed from output file;
+ * 4: using 'c++filt functions' to recover function names or disassemble instructions
+ * to locate crash code.
+ *
+ */
+
 void ShowStack()
 {
     int i;
